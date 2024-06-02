49fb59d1694cd72c204c2fde77b91bb09617a456
VOZNICHKA Maksym <maksym.voznicka@gmail.com>
2024-06-02 22:17:23 +0200

    feat(home page/Featured section)swiper done *without responsive design

--- js/main.js
+++ js/main.js
@@ -1,15 +1,72 @@
-// !Burger menu
+document.addEventListener('DOMContentLoaded', ()=>{
 
-(() => {
-	const burgerOpen = document.querySelector('.header__burger_menu')
-	const burgerClose = document.querySelector('.header__nav_close')
-	const navigation = document.querySelector('.header__nav')
+		// !Burger menu-----------------------------
 
-	burgerOpen.addEventListener('click', () => {
-		navigation.classList.toggle('active_menu')
-	})
+	(() => {
+		const burgerOpen = document.querySelector('.header__burger_menu')
+		const burgerClose = document.querySelector('.header__nav_close')
+		const navigation = document.querySelector('.header__nav')
 
-	burgerClose.addEventListener('click', () => {
-		navigation.classList.remove('active_menu')
-	})
-})();
+		burgerOpen.addEventListener('click', () => {
+			navigation.classList.toggle('active_menu')
+		})
+
+		burgerClose.addEventListener('click', () => {
+			navigation.classList.remove('active_menu')
+		})
+	})();
+
+	// !SWIPER-------------------------------------
+
+	const btnRight = document.querySelector('.featured__btn.right');
+	const btnLeft = document.querySelector('.featured__btn.left');
+	const cards = document.querySelectorAll('.featured__card');
+
+	let counter = 0;
+	
+	btnRight.addEventListener('click', () => {
+		counter++;
+		btnLeft.classList.add('onClick');
+		
+		if (counter >= 6){
+			counter = 0;
+			btnLeft.classList.remove('onClick');
+		}
+		
+		for(let i=0; i < cards.length; i++){
+			if(i !== counter){
+				cards[i].classList.add('hidden');
+			}
+			else{
+				cards[i].classList.remove('hidden');
+			}
+		}
+		
+	});
+
+	btnLeft.addEventListener('click', () => {
+
+		if(counter !== 0){
+			counter--;
+			
+			for(let i=0; i < cards.length; i++){
+				if(i !== counter ){
+					cards[i].classList.add('hidden');
+				}
+				else{
+					cards[i].classList.remove('hidden');
+				}
+			}
+			if(counter === 0){
+				btnLeft.classList.remove('onClick');
+			}
+		}
+	});
+
+
+
+
+
+});
+
+
