26eb8bad511409bbd45f63003ddea846b9b2bbe2
VOZNICHKA Maksym <maksym.voznicka@gmail.com>
2024-06-15 20:27:02 +0200

    feat(property Serenity Villa page) swiper done

--- js/main.js
+++ js/main.js
@@ -69,7 +69,22 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 	// !SWIPER SECOND----------------------------
 
+	const swiper = new Swiper('.property__swiper', {
+		// Optional parameters
+		direction: 'horizontal',
+		loop: true,
 	
+		// If we need pagination
+		pagination: {
+			el: '.swiper-pagination',
+		},
+	
+		// Navigation arrows
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
 
 });
 
