ed6ee46da6b58edd7fbe5a630dbc468af026fff4
VOZNICHKA Maksym <maksym.voznicka@gmail.com>
2024-07-18 16:59:07 +0200

    feat: (Admin Leads Page) Added functionality for displaying messages as a popupÐ

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -168,4 +168,37 @@ document.addEventListener('DOMContentLoaded', ()=>{
     }
 
 
+//     !SHOW MESSAGE ---------------------------
+    const messages = document.querySelectorAll('.contacts-lead-message');
+
+    if(messages){
+        messages.forEach(message =>{
+            message.addEventListener('click', function () {
+                const messageContent = this.innerText;
+                document.querySelector('#modalContainer').innerHTML = `
+        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        ${messageContent}
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+                let myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
+                myModal.show();
+            });
+        });
+    }
+
+
+
 });
